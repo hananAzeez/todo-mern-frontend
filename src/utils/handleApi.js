52b59b4b4976c3e6aae5ebbb42b51f5b943ae67a
hananAzeez <hanan.azeez112@gmail.com>
2023-04-08 15:19:01 +0530

    backend baseUrl updated

--- src/utils/handleApi.js
+++ src/utils/handleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://todo-mern-app-backend.onrender.com";
 
 const getAllTodo = (setTodo) => {
   axios.get(baseUrl).then(({ data }) => {
